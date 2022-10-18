1067170ecdc72a1b10684441596b29c72a14a515
DurianSorbet <neirobungin@gmail.com>
2022-10-18 21:08:57 +0700

    Connect React and Django

---
+++ ufcpredictor/ufcpredictor_web/predictor/frontend/src/App.js
@@ -0,0 +1,24 @@
+
+import React, { useState, useEffect } from 'react';
+//import './App.css';
+
+
+//<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js"/>
+
+function App() {
+
+  return (
+    <div>
+      <h1> App </h1>
+      <h3> Red Fighter </h3>
+      <input type="text"></input>
+      <h3> Blue Fighter </h3>
+      <input type="text"></input>
+      <h3> </h3>
+      <button>Predict</button>
+      
+    </div>
+  );
+}
+
+export default App;
