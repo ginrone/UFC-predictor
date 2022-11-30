e925ec78169bcb6375859b3a1dc65475bf49be51
DurianSorbet <neirobungin@gmail.com>
2022-11-30 17:21:59 +0700

    Added tensorflow model js to react

--- ufcpredictor/ufcpredictor_web_react/ufc-predictor/src/App.js
+++ ufcpredictor/ufcpredictor_web_react/ufc-predictor/src/App.js
@@ -1,6 +1,17 @@
 import './App.css';
+import * as tf from '@tensorflow/tfjs';
+
+async function runModel(){
+
+  modelUrl = "https://raw.githubusercontent.com/ginrone/UFC-predictor/master/ufcpredictor/ufcpredictor_machinelearning/models/model.json";
+  const model = await tf.loadGraphModel(modelUrl);
+  console.log("model is loaded")
+}
 
 function App() {
+
+  runModel();
+
   return (
     <div className="App">
       <h1> This is just testing </h1>
