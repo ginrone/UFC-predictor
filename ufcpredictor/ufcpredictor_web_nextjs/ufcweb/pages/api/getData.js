8f2a9c91f0893e393959945ff081a6b77d59b70d
DurianSorbet <neirobungin@gmail.com>
2023-02-23 13:34:43 +0700

    delete unnecessary files

--- ufcpredictor/ufcpredictor_web_nextjs/ufcweb/pages/api/getData.js
+++ ufcpredictor/ufcpredictor_web_nextjs/ufcweb/pages/api/getData.js
@@ -2,30 +2,43 @@ import cheerio from 'cheerio'
 
 const getData = async (req, res) => {
 
-    try {
-        const received = req.query.toSend.split(",")
+    const received = req.query.toSend.split(",")
+    let toBack = "wrong"
 
-        const name1 = received[0]
-        const name2 = received[1]
+    const name1 = received[0]
+    const name2 = received[1]
 
-        const str1 = `https://www.ufc.com/athlete/${name1}`
-        const resp1 = await fetch(str1)
-        const text1 = await resp1.text()
+    const str1 = `https://www.ufc.com/athlete/${name1}`
+    const resp1 = await fetch(str1)
 
-        const str2 = `https://www.ufc.com/athlete/${name2}`
-        const resp2 = await fetch(str2)
-        const text2 = await resp2.text()
-        
-        let $ = cheerio.load(text2)
-        let test = $('.e-chart-circle__percent').text()
-
-        console.log(test)
-        
-        return res.status(200).json({ test })
-
-    } catch (error) {
-        console.log(error)
+    if(resp1.status == 404){
+        return res.status(200).json({ toBack })
     }
+
+    const text1 = await resp1.text()
+
+    const str2 = `https://www.ufc.com/athlete/${name2}`
+    const resp2 = await fetch(str2)
+
+    if(resp2.status == 404){
+        return res.status(200).json({ toBack })
+    }
+
+    const text2 = await resp2.text()
+    
+    let a1 = cheerio.load(text1)
+
+    const lowerData1 = a1('.c-bio__text')
+    
+    console.log(lowerData1)
+
+
+    let a2 = cheerio.load(text2)
+
+    toBack = "asdasdsad"
+    
+    return res.status(200).json({ toBack })
+
 
 }
 
--- ufcpredictor/ufcpredictor_web_nextjs/ufcweb/pages/index.js
+++ ufcpredictor/ufcpredictor_web_nextjs/ufcweb/pages/index.js
@@ -1,5 +1,8 @@
+import * as tf from '@tensorflow/tfjs';
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+
 import { Input,Button } from '@chakra-ui/react'
 import {
   Alert,
@@ -7,23 +10,45 @@ import {
   AlertTitle,
   AlertDescription,
 } from '@chakra-ui/react'
+import { Select } from '@chakra-ui/react'
+
+import {
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from '@chakra-ui/react'
+
 
 export default function Home() {
 
-  const checkData = () => {
+  const checkData = async() => {
     var f1 = document.getElementById("f1").value
     var f2 = document.getElementById("f2").value
+    var divSel = document.getElementById("divSel").value
 
-    if(f1===""||f2===""){
-      document.getElementById("errorEmpty").hidden = false
+    if(divSel===""){
+      document.getElementById("errorDivision").hidden = false
     }
     else{
-      document.getElementById("errorEmpty").hidden = true
-      
-      const returnVal = getData();
-
-      if(returnVal==2){
-
+      document.getElementById("errorDivision").hidden = true
+      if(f1===""||f2===""){
+        document.getElementById("errorEmpty").hidden = false
+      }
+      else{
+        document.getElementById("errorEmpty").hidden = true
+        
+        const returnVal = await getData();
+  
+        if(returnVal.toBack=='wrong'){
+          document.getElementById("errorFalse").hidden = false
+        }
+        else{
+          document.getElementById("errorFalse").hidden = true
+  
+          // loadModel(dataInput);
+        }
       }
     }
   }
@@ -38,9 +63,12 @@ export default function Home() {
     const res = await fetch('/api/getData?toSend='+toSend)
     const dl = await res.json()
 
-    console.log(dl)
+    return dl;
+  }
 
-    return 2;
+  const loadModel = async(dataInput) => {
+    const model = await tf.loadLayersModel("https://raw.githubusercontent.com/ginrone/UFC-predictor/master/ufcpredictor/ufcpredictor_machinelearning/models/model.json");
+    console.log("model loaded")
   }
 
   return (
@@ -56,6 +84,37 @@ export default function Home() {
         </h1>
         <Input placeholder='Fighter 1' id='f1' />
         <Input placeholder='Fighter 2' id='f2'/>
+
+        <NumberInput defaultValue={2000} min={2000}>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+
+        <Select placeholder='Select Division' id='divSel'>
+          <option value='o1'>Flyweight (125)</option>
+          <option value='o2'>Bantamweight (135)</option>
+          <option value='o3'>Featherweight (145)</option>
+          <option value='o4'>Lightweight (155)</option>
+          <option value='o5'>Welterweight (170)</option>
+          <option value='o6'>Middleweight (185)</option>
+          <option value='o7'>Light Heavyweight (205)</option>
+          <option value='o8'>Heavyweight (265)</option>
+          <option value='o9'>Women's Strawweight (115)</option>
+          <option value='o10'>Women's Flyweight (125)</option>
+          <option value='o11'>Women's Bantamweight (135)</option>
+          <option value='o12'>Women's Featherweight (145)</option>
+        </Select>
+
+        <div id="errorDivision" hidden={true}>
+          <Alert status='error'>
+            <AlertIcon />
+            <AlertTitle>Division has not been chosen</AlertTitle>
+            <AlertDescription>Please select the division</AlertDescription>
+          </Alert>
+        </div>
 
         <div id="errorEmpty" hidden={true}>
           <Alert status='error'>
